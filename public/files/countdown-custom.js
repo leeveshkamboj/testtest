26f01eb439ca39d6d9cab5540ff33463da663fe4
Leevesh Kamboj <leeveshk12@gmail.com>
2023-04-24 23:03:00 +0530

    fix

---
+++ public/files/countdown-custom.js
@@ -0,0 +1,6 @@
+jQuery(document).ready(function() {
+        $(function () {
+            $('#defaultCountdown').countdown({until: new Date(2023, 4, 10, 0)}); // year, month, date, hour
+        });
+});		
+
